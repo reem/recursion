03ca47a37e43a1811325e54023e45ba10e31440e
Jonathan Reem <jonathan.reem@gmail.com>
2014-03-29 17:17:05 -0400

    Implemented stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,7 +1,40 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
 
+function stringifyObject(obj) { // Part of a mutually recursive pair.
+    var inside, key, stringifiedVal;
+    inside = "";
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            stringifiedVal = stringifyJSON(obj[key]);
+            if (stringifiedVal !== "") {
+                inside = inside.concat('"' + key + '":' +
+                                       stringifiedVal + ',');
+            }
+        }
+    }
+    inside = inside.substring(0, inside.length - 1);
+    return "{" + inside + "}";
+}
+
 // but you don't so you're going to have to write it from scratch:
-var stringifyJSON = function (obj) {
-  // your code goes here
-};
+function stringifyJSON(obj) {
+    switch (typeof obj) {
+        case "object":
+            if (Array.isArray(obj)) {
+                return "[" + obj.map(stringifyJSON).join(',') + "]";
+            } if (obj === null) {
+                return "null";
+            } return stringifyObject(obj);
+        case "boolean":
+            if (obj) {
+                return "true";
+            } return "false"; // No need for break
+        case "number":
+            return obj.toString();
+        case "string":
+            return '"' + obj + '"';
+        default:
+            return "";
+    }
+}
