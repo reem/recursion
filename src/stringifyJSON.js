49832d0c5668d10bf5ef9592ce0caca86ad26fda
Hack Reactor Students <jonathan.reem@gmail.com>
2014-04-14 15:46:19 -0700

    Added better, paired version of stringifyJSON.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,38 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+  switch (typeof obj) {
+    case "object":
+      if (Array.isArray(obj)) {
+        return "[" + obj.map(stringifyJSON).join(',') + "]";
+      } if (obj === null) {
+        return "null";
+      } if (Object.keys(obj).length === 0) {
+        return "{}";
+      } return stringifyObject(obj);
+    case "boolean":
+      if (obj) {
+        return "true";
+      } return "false"; // No need for break
+    case "number":
+      return obj.toString();
+    case "string":
+      return '"' + obj + '"';
+    default:
+      return "";
+  }
+};
+
+var isStringifiable = function (obj) {
+  return _.contains(["object", "boolean", "number", "string"], typeof obj);
+};
+
+var stringifyObject = function (obj) {
+  return '{' + _.chain(obj).pairs()
+    .filter(function (pair) {
+      return isStringifiable(pair[1])
+    })
+    .map(function (pair) {
+      return stringifyJSON(pair[0]) + ':' + stringifyJSON(pair[1]);
+    }).value().join(',') + '}';
 };
